1c77c72ca69f8eccc99cbab51f46eec394e34da6
Julian de Villiers <juliandv96@gmail.com>
2016-09-22 17:55:56 +1200

    Changed to neopixel twinkling

--- TwitterBlink/Incomming_Tweet.ino
+++ TwitterBlink/Incomming_Tweet.ino
@@ -1,44 +1,56 @@
-// do something when a tweet comes in
+/**
+ * Twinkle lights when tweet comes in - Phase 2
+ */
 
 void newTweet(){
 
-  tone(buzzer, 1000); // Send 1KHz sound signal...
-    digitalWrite(led, 1);
-    delay(50);
-    digitalWrite(led, 0);
-   noTone(buzzer);     // Stop sound..
+if (random(20) == 1) {
+    uint16_t i = random(Pixels);                       //Select one random pixel
+    if (greenStates[i] < 1 && blueStates[i] < 1) {     //Is it on or off. If off, turn on with specific colour
+      greenStates[i] = 50;                             //Sets how much of each colour/brightness
+      blueStates[i] = 50;
+    }
+  }
 
+  for (uint16_t l = 0; l < Pixels; l++) {
+    if (greenStates[l] > 1 || blueStates[l] > 1) {
+      strip_1.setPixelColor(l, 0, greenStates[l], blueStates[l]);
+      strip_2.setPixelColor(l, 0, greenStates[l], blueStates[l]);
+      strip_3.setPixelColor(l, 0, greenStates[l], blueStates[l]);
+      strip_4.setPixelColor(l, 0, greenStates[l], blueStates[l]);
 
+/**
+ * Fade until light is off (0)
+ */
+      if (greenStates[l] > 1) {
+        greenStates[l] = greenStates[l] * fadeRate;
+      } else {
+        greenStates[l] = 0;
+      }
 
+      if (blueStates[l] > 1) {
+        blueStates[l] = blueStates[l] * fadeRate;
+      } else {
+        blueStates[l] = 0;
+      }
 
+    }
+    else {                                     //Set colour to black/"off"
+      strip_1.setPixelColor(l, 0, 0, 0);
+      strip_2.setPixelColor(l, 0, 0, 0);
+      strip_3.setPixelColor(l, 0, 0, 0);
+      strip_4.setPixelColor(l, 0, 0, 0);
+    }
+  }
+  strip_1.show();                             //Show colours
+  strip_2.show();
+  strip_3.show();
+  strip_4.show();
 
-// Yay pretty colours!
-
-  colorWipe(strip.Color(155, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 155, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 155), 50); // Blue
-  colorWipe(strip.Color(0, 0, 0, 155), 50); // White
-
-strip.show();
-
-
-
-
+  delay(1);                                    //Rate of twinkle. Lower number, faster twinkle.
 
   // and resets the go variable to 0
   // Keep this line!
   Bridge.put("go", String(0));
 }
 
-
-
-
-
-
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
