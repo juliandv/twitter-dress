30f1693bffd680b201b92e22c1acb13cb5457a6f
juliandv <juliandv96@gmail.com>
2016-09-23 22:33:18 +1200

    commented out button code

--- TwitterBlink/States.ino
+++ TwitterBlink/States.ino
@@ -1,22 +1,27 @@
 /**
  * Checks pin connection as switch and changes phases accordingly
  */
-void checkSwitches() {
-  phase1  = digitalRead(PHASE_1_BTN);       // read buttons
-  phase2 = digitalRead(PHASE_2_BTN);
-
-    if ( phase1 == HIGH ) {     //If connection is made, switch phases
-    phase = 1;  //Constant twinkle
-  }
-
-  if ( phase1 == LOW ) {
-    phase = 2;  //Twitter
-  }
-
-  if ( phase2 == HIGH ) {
-    phase = 3;  //Off
-  }
-}
+//void checkSwitches() {
+//  phase1  = digitalRead(PHASE_1_BTN);
+//  // read buttons
+//  phase2 = digitalRead(PHASE_2_BTN);
+//
+//    if ( phase1 == LOW ) {
+//      //If connection is made, switch phases
+//    phase = 1;
+//    //Constant twinkle
+//  }
+//
+//  if ( phase1 == HIGH ) {
+//    phase = 2;
+//    //Twitter
+//  }
+//
+//  if ( phase2 == LOW ) {
+//    phase = 3;
+//    //Off
+//  }
+//}
 
 /**
  * Takes number of phase and executes
