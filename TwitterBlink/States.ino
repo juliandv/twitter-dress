82ef13a6510d185118c6ab0e53d317eb634bbf94
juliandv <juliandv96@gmail.com>
2016-09-28 21:35:45 +1300

    bug fixes and testing

--- TwitterBlink/States.ino
+++ TwitterBlink/States.ino
@@ -31,12 +31,15 @@ void switchPhases() {
   switch ( phase )
   {
     case 1: //Constant Twinkle
+      Serial.println("CONSTANT TWINKLE STATE");
+
       twinkle();
       break;
     case 2: //Twitter
+      Serial.println("TWITTER STATE");
+
       //Read the "go" Bridge var and copies it into the go_buffer variable
       Bridge.get("go", go_buffer, 2);
-
       //Convert the bugger into an int variable (should I have used bool instead?)
       go = atoi(go_buffer);
 
@@ -44,17 +47,27 @@ void switchPhases() {
 
         // get current time since startup
         unsigned long currentMillis = millis();
+        unsigned long twinkleUntil = currentMillis + twinkleLength;
+        Serial.println(currentMillis);
         // loop twinkle code for twinkleLength mills
-        if (currentMillis <= currentMillis + twinkleLength) {
+        while(currentMillis <= twinkleUntil) {
+            Serial.println("New tweet! Twinkle...");
             twinkle();
+            currentMillis = millis();
         }
+          strip_1.show();
+          strip_2.show();
+          strip_3.show();
+          strip_4.show();
         Bridge.put("go", String(0));   // reset the new tweet 'go' variable to 0
       } else {
         delay(30); // or else wait a bit then check if there is a new tweet
       }
       break;
     case 3: //Off
+      Serial.println("OFF STATE");
       // Do nothing
+      delay(10);
       break;
   }
 }
