a895eea83d426ee5d1669e759a52eb22966c7507
juliandv <juliandv96@gmail.com>
2016-09-28 16:22:41 +1300

    simplifying states

--- TwitterBlink/States.ino
+++ TwitterBlink/States.ino
@@ -1,13 +1,13 @@
 /**
- * Checks pin connection as switch and changes phases accordingly
- */
+   Checks pin connection as switch and changes phases accordingly
+*/
 void checkSwitches() {
   phase1  = digitalRead(PHASE_1_BTN);
   // read buttons
   phase2 = digitalRead(PHASE_2_BTN);
 
-    if ( phase1 == LOW ) {
-      //If connection is made, switch phases
+  if ( phase1 == LOW ) {
+    //If connection is made, switch phases
     phase = 1;
     //Constant twinkle
   }
@@ -24,15 +24,15 @@ void checkSwitches() {
 }
 
 /**
- * Takes number of phase and executes
- */
+   Takes number of phase and executes
+*/
 void switchPhases() {
 
-    switch ( phase )
+  switch ( phase )
   {
     case 1: //Constant Twinkle
-      constantTwinkle();
-    break;
+      twinkle();
+      break;
     case 2: //Twitter
       //Read the "go" Bridge var and copies it into the go_buffer variable
       Bridge.get("go", go_buffer, 2);
@@ -42,18 +42,19 @@ void switchPhases() {
 
       if (go == 1) {
 
-  unsigned long currentMillis = millis();
-
-  if (currentMillis <= currentMills + twinkleLength) {
-      newTweet();
-  }
-  Bridge.put("go", String(0));   // reset the new tweet 'go' variable to 0
+        // get current time since startup
+        unsigned long currentMillis = millis();
+        // loop twinkle code for twinkleLength mills
+        if (currentMillis <= currentMillis + twinkleLength) {
+          twinkle();
+        }
+        Bridge.put("go", String(0));   // reset the new tweet 'go' variable to 0
       } else {
-        delay(30); // or else wait
+        delay(30); // or else wait a bit then check if there is a new tweet
       }
       break;
-      case 3: //Off
-      //Do nothing
+    case 3: //Off
+      // Do nothing
       break;
   }
 }
