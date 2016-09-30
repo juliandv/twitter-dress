26505e2858bd851085837731845be27692df5354
juliandv <juliandv96@gmail.com>
2016-09-30 17:37:37 +1300

    Move turning off pixels code to its own method

---
+++ twitter-dress/turnPixelsOff.ino
@@ -0,0 +1,24 @@
+void turnPixelsOff() {
+  Serial.println("Turning all pixels off...");
+
+  for (uint16_t l = 0; l < Pixels_1; l++) {
+    greenStates1[l] = 0;
+    blueStates1[l] = 0;
+  }
+
+  for (uint16_t l = 0; l < Pixels_2; l++) {
+    greenStates2[l] = 0;
+    blueStates2[l] = 0;
+  }
+
+  for (uint16_t l = 0; l < Pixels_3; l++) {
+    greenStates3[l] = 0;
+    blueStates3[l] = 0;
+  }
+
+  for (uint16_t l = 0; l < Pixels_4; l++) {
+    greenStates4[l] = 0;
+    blueStates4[l] = 0;
+  }
+
+}
