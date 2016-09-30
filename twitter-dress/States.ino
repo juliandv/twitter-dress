b446ce29e38bbf96f88f18bb3a54bfd44245c9b9
juliandv <juliandv96@gmail.com>
2016-09-30 16:59:41 +1300

    Merge Claudines code

--- twitter-dress/States.ino
+++ twitter-dress/States.ino
@@ -50,16 +50,24 @@ void switchPhases() {
         unsigned long twinkleUntil = currentMillis + twinkleLength;
         Serial.println(currentMillis);
         // loop twinkle code for twinkleLength mills
-        while(currentMillis <= twinkleUntil) {
-            Serial.println("New tweet! Twinkle...");
-            twinkle();
-            currentMillis = millis();
+        while (currentMillis <= twinkleUntil) {
+          Serial.println("New tweet! Twinkle...");
+          twinkle();
+          currentMillis = millis();
         }
-          strip_1.show();
-          strip_2.show();
-          strip_3.show();
-          strip_4.show();
+
+        colorWipe1(strip_1.Color( 0, 0, 0), 50);
+        colorWipe2(strip_2.Color(0, 0, 0), 50);
+        colorWipe3(strip_3.Color(0,0,0), 50);
+        colorWipe4(strip_4.Color(0,0,0), 50);
+
+        strip_1.show();
+        strip_2.show();
+        strip_3.show();
+        strip_4.show();
+
         Bridge.put("go", String(0));   // reset the new tweet 'go' variable to 0
+
       } else {
         delay(30); // or else wait a bit then check if there is a new tweet
       }
@@ -69,5 +77,30 @@ void switchPhases() {
       // Do nothing
       delay(10);
       break;
+  }
+}
+
+void colorWipe1(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip_1.numPixels(); i++) {
+    strip_1.setPixelColor(i, c);
+    strip_1.show();
+  }
+}
+void colorWipe2(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip_2.numPixels(); i++) {
+    strip_2.setPixelColor(i, c);
+    strip_2.show();
+  }
+}
+void colorWipe3(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip_3.numPixels(); i++) {
+    strip_3.setPixelColor(i, c);
+    strip_3.show();
+  }
+}
+void colorWipe4(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip_4.numPixels(); i++) {
+    strip_4.setPixelColor(i, c);
+    strip_4.show();
   }
 }
